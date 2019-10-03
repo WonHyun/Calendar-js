747f4ca1343a85ce1b95710b3cdc8167263f9616
WonHyun <coy4728@naver.com>
2019-10-03 14:09:08 +0900

    현재 날짜를 선택하는 기능 추가

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,3 +1,9 @@
+const setCurrentMonth = targetDate => {
+  let date =
+    targetDate.getFullYear() + "년 " + (targetDate.getMonth() + 1) + "월";
+  $(".calendar-header > h4").text(date);
+};
+
 $(function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
@@ -21,19 +27,37 @@ $(".event-consecutive, .event, .event-repeated").click(function(event) {
 });
 
 $(function() {
+  $("#datepicker-today").datetimepicker({
+    autoClose: true,
+    useCurrent: true,
+    format: "YYYY-MM-DD"
+  });
   $("#datepicker-start").datetimepicker({
+    autoClose: true,
+    useCurrent: true,
     format: "YYYY-MM-DD"
   });
   $("#datepicker-end").datetimepicker({
+    autoClose: true,
+    useCurrent: true,
     format: "YYYY-MM-DD"
+  });
+  $("#datepicker-today").on("change.datetimepicker", function(e) {
+    setCurrentMonth(e.date._d);
   });
 });
 
 $(function() {
   $("#timepicker-start").datetimepicker({
+    useCurrent: true,
     format: "LT"
   });
   $("#timepicker-end").datetimepicker({
+    useCurrent: true,
     format: "LT"
   });
+});
+
+$(function() {
+  $(".calendar-header > h4").text(moment().format("YYYY[년] MM[월]"));
 });
