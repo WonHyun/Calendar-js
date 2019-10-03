a0512bd091606771c3b2fb966967b0f559c151e8
WonHyun <coy4728@naver.com>
2019-10-03 15:06:43 +0900

    schedule api 오류 수정

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const models = require("../models");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 router.get("/:year/:month", function(req, res, next) {
   let year = req.params.year;
   let month = req.params.month;
   let startDate = new Date(year, month - 1, -7);
   let lastDate = new Date(year, month, +7);
+
   models.Schedule.findAll({
     where: {
-      $or: [
-        {
-          startAt: {
-            between: [startDate, lastDate]
-          }
-        },
-        {
-          endAt: {
-            between: [startDate, lastDate]
-          }
-        }
+      [Op.or]: [
+        { startAt: { [Op.between]: [startDate, lastDate] } },
+        { endAt: { [Op.between]: [startDate, lastDate] } }
       ]
     }
   })
@@ -28,27 +23,26 @@ router.get("/:year/:month", function(req, res, next) {
     })
     .catch(function(err) {
       console.log(err);
-      res.json({ error: "db error", success: false });
+      res.json({ error: "db error: " + err, success: false });
     });
 });
 
 router.post("/write", function(req, res, next) {
-  models.post
-    .create({
-      title: req.body.title,
-      discription: req.body.discription,
-      createdAt: new Date(),
-      startAt: req.body.startAt,
-      endAt: req.endAt,
-      repeated: req.body.repeated
-    })
+  models.Schedule.create({
+    title: req.body.title,
+    discription: req.body.discription,
+    createdAt: new Date(),
+    startAt: req.body.startAt,
+    endAt: req.body.endAt,
+    repeated: req.body.repeated
+  })
     .then(result => {
       console.log("success");
       res.json({ success: true });
     })
     .catch(err => {
       console.log("fail, create schedule");
-      res.json({ error: "db error", success: false });
+      res.json({ error: "db error: " + err, success: false });
     });
 });
 
