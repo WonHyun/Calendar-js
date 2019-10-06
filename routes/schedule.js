30f04ba7cb7b77148e1cfcf0fe910ac1a8e36be8
WonHyun <coy4728@naver.com>
2019-10-06 22:16:57 +0900

    schedule update db 부분 추가

--- routes/schedule.js
+++ routes/schedule.js
@@ -4,6 +4,30 @@ const models = require("../models");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
+router.put("/update/:id", function(req, res, next) {
+  let id = parseInt(req.params.id);
+
+  models.Schedule.update(
+    {
+      title: req.body.title,
+      description: req.body.description,
+      startAt: req.body.startAt,
+      endAt: req.body.endAt,
+      repeated: req.body.repeated
+    },
+    {
+      where: { scheduleId: id }
+    }
+  )
+    .then(result => {
+      res.json({ success: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({ error: "db error: " + err, success: false });
+    });
+});
+
 router.get("/delete/:id", function(req, res, next) {
   let id = parseInt(req.params.id);
   models.Schedule.destroy({
